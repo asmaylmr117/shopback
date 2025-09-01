67a05923139a2a728dd136952091d2a4c232a966
Mostafa Ismail Alanani <asmaylmr117@gmail.com>
2025-09-01 19:48:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const { pool } = require('./config/database');
-const { authenticateToken, requireAdmin } = require('../middleware/auth');
+const { authenticateToken, requireAdmin } = require('./middleware/auth');
 
 const app = express();
 
